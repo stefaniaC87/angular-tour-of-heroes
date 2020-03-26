914306b72e25f69c697d1cd1edc8ea9f933ff253
Stefania <stefaniacappellino@gmail.com>
2020-03-26 13:14:31 +0100

    aggiunto e mdificato file

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
-  constructor() { }
 
-  ngOnInit(): void {
+constructor(
+  private heroService: HeroService
+  ) {}
+
+  ngOnInit(){
     /*let scope = this;
     setTimeout(function (){
       scope.hero.name = 'Stefania';
     }, 5000);*/
+    this.getHeroes();
   }
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
